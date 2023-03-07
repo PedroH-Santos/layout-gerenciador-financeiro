547821a0062dfed8c6f00edae808117749355356
PedroH-Santos <pedrohenriquesantos0303@gmail.com>
2023-03-07 14:08:57 -0300

    Finalizacao refresh token

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,13 +1,13 @@
 import { createContext, ReactElement, useEffect, useState } from "react";
 import { setCookie, parseCookies } from "nookies";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import { api } from "@/services/axios";
 import { User } from "@/@types/User";
 export const AuthContext = createContext({} as AuthContextType);
 
 interface AuthContextType {
     isAuthenticated: Boolean;
-    user: User | null;
+    user: User;
     signIn: (data: SignInData) => Promise<void>
 
 }
@@ -21,44 +21,84 @@ interface SignInData {
     password: string;
 }
 
+    
 interface ResponseToken {
     access_token: string;
     refresh_token: string;
+    user: {
+        userId: string;
+        userName: string;
+    };
+}
+
+interface ResponseGetNewUser {
     user: User;
 }
 
 
 export function AuthProvider({ children }: AuthContextProps) {
 
-    const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
     async function signIn({ username, password }: SignInData) {
         const response = await api.post<ResponseToken>('/login', {
             username,
             password
         });
-        setCookie(undefined, 'managerFinancial.token', response.data.access_token, {
-            maxAge: 60 * 15 //15 minutes
-        });
-        setCookie(undefined, 'managerFinancial.refreshToken', response.data.refresh_token, {
-            maxAge: 60 * 60 * 24 //1 hour
-        });
-        setUser(response.data.user);
+        createCookiesToken(response.data);
         Router.push('/group/list');
     }
 
+
+    function createCookiesToken(data: ResponseToken){
+        setCookie(undefined, 'managerFinancial.token', data.access_token, {
+            maxAge: 60 * 15, //15 minutes
+            path: '/'
+        });
+        setCookie(undefined, 'managerFinancial.refreshToken', data.refresh_token, {
+            maxAge: 60 * 60 * 24, //1 hour
+            path: '/'
+        });
+        getNewUser(data.user.userId,data.access_token);
+    }
+
+    async function requestRefreshToken(refreshToken: string){
+        const newToken = await api.get<ResponseToken>('/login/refresh', {
+            headers: { 'Authorization': 'Bearer ' + refreshToken },
+        })
+        createCookiesToken(newToken.data);
+    }
+
+    async function getNewUser(id: string | undefined, token: string){
+        if(id){
+            const response = await api.get<ResponseGetNewUser>(`/users/one/${id}`, {
+                headers: { 'Authorization': 'Bearer ' + token },
+            });
+            setUser(response.data.user);
+            console.log(user);
+            console.log(response.data.user);
+        } 
+
+    }
     useEffect(() => {
         const { 'managerFinancial.token': token } = parseCookies();
         if (token) {
             //buscar os dados do usuário atualizado backend
+            getNewUser(user?.id,token);
         }else{ 
             const { 'managerFinancial.refreshToken': refreshToken } = parseCookies();
-            //fazer lógica para pegar um novo token
-            
-        }   
+            if (refreshToken) {
+                requestRefreshToken(refreshToken);   
+            }else {
+                Router.push('/login');
+            }
 
+        }  
+        console.log(user); 
     }, []);
 
+
+    
     return (
         <AuthContext.Provider value={{ user, signIn, isAuthenticated }}>
             {children}
--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
 import getConfig from 'next/config';
+import { parseCookies } from "nookies";
 
 const { publicRuntimeConfig } = getConfig();
 
-export const api = axios.create({
-    baseURL: publicRuntimeConfig.apiURL,
-})
+export function getApiClient(ctx?: any){
+    const { 'managerFinancial.token': token } = parseCookies(ctx);
+    const api = axios.create({
+        baseURL: publicRuntimeConfig.apiURL,
+    })
+    if (token) {
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
+
+    return api;
+}
+
+export const api = getApiClient();
